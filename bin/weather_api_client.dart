062544ab71ecc44253eb90747bd532c72c82c4af
Dima Liubko <d.o.liubko@gmail.com>
2021-11-23 11:18:40 +0200

    Add getLocationId method

--- bin/weather_api_client.dart
+++ bin/weather_api_client.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
 class WeatherApiClient {
@@ -7,5 +9,13 @@ class WeatherApiClient {
     final locationUrl = Uri.parse('$baseUrl/location/search/?query=$city');
 
     final locationResponse = await http.get(locationUrl);
+
+    if (locationResponse.statusCode != 200) {
+      throw Exception('Error getting locationId for city: $city');
+    }
+
+    final locationJson = jsonDecode(locationResponse.body) as List;
+
+    return locationJson.first['woeid'] as int;
   }
 }
