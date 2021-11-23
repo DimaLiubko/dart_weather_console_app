d5864c14049e5c2580c25195a43c0f7bcb7f7480
Dima Liubko <d.o.liubko@gmail.com>
2021-11-23 10:44:52 +0200

    Initial commit

---
+++ bin/dart_weather_console_app.dart
@@ -0,0 +1,5 @@
+import 'package:dart_weather_console_app/dart_weather_console_app.dart' as dart_weather_console_app;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_weather_console_app.calculate()}!');
+}
